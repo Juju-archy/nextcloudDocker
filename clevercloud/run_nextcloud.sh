3e7f084dd3280621889c828d615868ee50f09513
Juju-archy <pouny.julie@gmail.com>
2024-09-09 11:56:10 +0200

    test

--- clevercloud/run_nextcloud.sh
+++ clevercloud/run_nextcloud.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-# Variables d'environnement pour la connexion à PostgreSQL
-POSTGRESQL_ADDON_DB=${POSTGRESQL_ADDON_DB}
-POSTGRESQL_ADDON_HOST=${POSTGRESQL_ADDON_HOST}
-POSTGRESQL_ADDON_PASSWORD=${POSTGRESQL_ADDON_PASSWORD}
-POSTGRESQL_ADDON_PORT=${POSTGRESQL_ADDON_PORT}
-POSTGRESQL_ADDON_USER=${POSTGRESQL_ADDON_USER}
+# Vérification des variables d'environnement
+if [ -z "$POSTGRESQL_ADDON_DB" ] || [ -z "$POSTGRESQL_ADDON_HOST" ] || [ -z "$POSTGRESQL_ADDON_PASSWORD" ] || [ -z "$POSTGRESQL_ADDON_PORT" ] || [ -z "$POSTGRESQL_ADDON_USER" ] || [ -z "$NEXT_USER" ] || [ -z "$NEXT_PSWD" ]; then
+    echo "Missing environment variables. Please ensure all required variables are set."
+    exit 1
+fi
 
 # Démarrer le conteneur Nextcloud avec les variables d'environnement
 docker run -d -p 8080:80 \
@@ -17,6 +16,5 @@ docker run -d -p 8080:80 \
   -e NEXTCLOUD_DB_NAME=${POSTGRESQL_ADDON_DB} \
   -e NEXTCLOUD_DB_USER=${POSTGRESQL_ADDON_USER} \
   -e NEXTCLOUD_DB_PASSWORD=${POSTGRESQL_ADDON_PASSWORD} \
-  -e NEXTCLOUD_DB_SSL_MODE=require \ 
+  -e NEXTCLOUD_DB_SSL_MODE=disable \
   nextcloud
-  
